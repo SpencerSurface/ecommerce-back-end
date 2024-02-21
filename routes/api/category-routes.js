4c5683b75f38a1df927e76fdeda5fd8e4009106c
Spencer Surface <54449845+SpencerSurface@users.noreply.github.com>
2024-02-21 16:05:25 -0500

    add route to update a category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -43,8 +43,14 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const categoryData = await Category.update(req.body, {where: {id: req.params.id}});
+    res.json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
