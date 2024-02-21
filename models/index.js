b6dcf5dd448a368cbc2c2a74f70c676dcb26a930
Spencer Surface <54449845+SpencerSurface@users.noreply.github.com>
2024-02-21 16:20:40 -0500

    specify custom foreign keys in Product-Tag association

--- models/index.js
+++ models/index.js
@@ -8,29 +8,31 @@ const ProductTag = require('./ProductTag');
 Product.belongsTo(Category, {
   foreignKey: "category_id",
   onDelete: "CASCADE"
-})
+});
 
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: "category_id",
   onDelete: "CASCADE"
-})
+});
 
 // Products belongsToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: {
     model: ProductTag,
     unique: false
-  }
-})
+  },
+  foreignKey: "product_id"
+});
 
 // Tags belongsToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: {
     model: ProductTag,
     unique: false
-  }
-})
+  },
+  foreignKey: "tag_id"
+});
 
 module.exports = {
   Product,
